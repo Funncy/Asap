8fd54792540f1fb46c7fbdedd0e55c889162e8a0
Funncy <gywns6222@gmail.com>
2018-02-13 21:31:41 +0900

    test11

--- show_seattable.php
+++ show_seattable.php
@@ -16,35 +16,30 @@
     require_once __DIR__ . '/db_connection.php';
  
 
-    // echoing JSON response
-    echo json_encode($response);
 
     // connecting to db
     $db = new DB_CONNECT();
- 
-
-    // echoing JSON response
-    echo json_encode($response);
+ echo "start1";
 
     echo $db;
        // echoing JSON response
-       echo json_encode($response);
+       echo "start2";
     // mysql inserting a new row
 //$result = mysql_query("INSERT INTO products(name, price, description) VALUES('$name', '$price', '$description')");
     $result = mysql_query("SELECT * FROM PcSeatTable WHERE ID = 1");
        // echoing JSON response
-       echo json_encode($response);
+       echo "start3";
     if(empty($result)){
         echo "empty";
     }
    // echoing JSON response
-   echo json_encode($response);
+   echo "start4";
     $SeatTable = array();
     $SeatTable["row"] = $result["row"];
     $SeatTable["col"] = $result["col"];
     $SeatTable["SeatData"] = $result["SeatData"];
    // echoing JSON response
-   echo json_encode($response);
+   echo "start5";
     echo json_encode($SeatTable);
 
 
