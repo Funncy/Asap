85394401802780ec7439af8cd7ad57215343ea5c
Funncy <gywns6222@gmail.com>
2018-02-13 21:07:02 +0900

    db_connection

---
+++ db_config.php
@@ -0,0 +1,12 @@
+
+<?php
+ 
+/*
+ * All database connection variables
+ */
+ 
+define('DB_USER', "root"); // db user
+define('DB_PASSWORD', "cndehdcks$04"); // db password (mention your db password here)
+define('DB_DATABASE', "asap"); // database name
+define('DB_SERVER', "http://203.250.148.50"); // db server
+?>
---
+++ show_seattable.php
@@ -0,0 +1,27 @@
+<?php
+ 
+/*
+ * Following code will create a new product row
+ * All product details are read from HTTP Post Request
+ */
+ 
+
+
+    // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+ 
+    // connecting to db
+    $db = new DB_CONNECT();
+ 
+    // mysql inserting a new row
+//$result = mysql_query("INSERT INTO products(name, price, description) VALUES('$name', '$price', '$description')");
+    $result = mysql_query("SELECT * FROM PcSeatTable WHERE ID = 1");
+    
+    $SeatTable = array();
+    $SeatTable["row"] = $result["row"];
+    $SeatTable["col"] = $result["col"];
+    $SeatTable["SeatData"] = $result["SeatData"];
+
+    echo json_encode($SeatTable);
+ 
+?>
