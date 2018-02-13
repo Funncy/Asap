f0bbe783141fcbca1c4270441468a7f8f2b38e8a
Funncy <gywns6222@gmail.com>
2018-02-13 21:29:58 +0900

    test9

--- show_seattable.php
+++ show_seattable.php
@@ -11,15 +11,21 @@
     $response["success"] = 0;
     $response["message"] = "Required field(s) is missing";
  
-    // echoing JSON response
-    echo json_encode($response);
 
     // include db connect class
     require_once __DIR__ . '/db_connection.php';
  
+
+    // echoing JSON response
+    echo json_encode($response);
+
     // connecting to db
     $db = new DB_CONNECT();
  
+
+    // echoing JSON response
+    echo json_encode($response);
+
     echo $db;
     // mysql inserting a new row
 //$result = mysql_query("INSERT INTO products(name, price, description) VALUES('$name', '$price', '$description')");
