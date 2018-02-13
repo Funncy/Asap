52914b56eb12031539d9d1be2e8b67afc2c903aa
Funncy <gywns6222@gmail.com>
2018-02-13 21:22:49 +0900

    test6

--- db_config.php
+++ db_config.php
@@ -8,5 +8,5 @@
 define('DB_USER', "root"); // db user
 define('DB_PASSWORD', "cndehdcks$04"); // db password (mention your db password here)
 define('DB_DATABASE', "asap"); // database name
-define('DB_SERVER', "203.250.148.50"); // db server
+define('DB_SERVER', "localhost"); // db server
 ?>
